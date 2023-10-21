6ac75a96131c95f279e1e698ac74a121a2ad92ca
Hamza <110095038+HamzaHayee36@users.noreply.github.com>
2023-10-21 10:44:53 +0500

    EventDetailActivity Screen Completed

--- app/src/main/java/com/example/mapapp/EventDetailActivity.java
+++ app/src/main/java/com/example/mapapp/EventDetailActivity.java
@@ -1,10 +1,14 @@
 package com.example.mapapp;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +18,21 @@ public class EventDetailActivity extends AppCompatActivity {
 
     private boolean isProfileArrowUp = false;
     private boolean isBellArrowUp = false;
+
+    private String[][] data = {
+            {"Department", "EventTitle", "Date", "Description"},
+            {"Computer\nScience", "Gaming Event", "15-\nApril-\n2023", "Gaming conventions are events where people interested in video games, \n" +
+                    "board games, and other types of gaming" +
+                    "\n can come together to play and meet other like-minded people"},
+            {"Software\nEngineering", "Electronic\nEntertainment", "10-\nApril-\n2023", "Entertainment that someone is able to see or hear using the internet: \n" +
+                    "We hope that the expansion of broadband will make \ne-entertainment and e-commerce available to all."},
+            {"Computer\nScience", "TTRPG Event", "06-\nApril-\n2023", "Events are the mechanisms for carrying out various processes within a game. \n" +
+                    "For example, you can create an Event that assigns an image and text\n" +
+                    " to a character which will then appear on the screen when a player talks to that character."},
+            {"Software\nEngineering", "Programming\nCompetition", "25-\nMarch-\n2023", "Many platforms conduct coding contests, but Microsoft Imagine cup,\n" +
+                    " Google coding competitions,\n" +
+                    " Codeforces and TopCoder are few competitions among the best."}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +68,43 @@ public class EventDetailActivity extends AppCompatActivity {
                 isBellArrowUp = !isBellArrowUp;
             }
         });
-    }
+
+
+        GridLayout gridLayout = findViewById(R.id.gridLayout);
+
+        // Loop through the data and add it to the GridLayout
+        for (int row = 0; row < data.length; row++) {
+            for (int col = 0; col < data[row].length; col++) {
+                TextView textView = new TextView(this);
+                textView.setText(data[row][col]);
+
+                // Style the headers
+                if (row == 0) {
+                    textView.setTextColor(Color.BLACK);
+                    textView.setTypeface(null, Typeface.BOLD);
+                }
+
+                textView.setPadding(16, 16, 16, 16);
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+                gridLayout.addView(textView, params);
+            }
+
+            // Add a dark grey separator line after every row, except the last one
+            if (row < data.length - 1) {
+                View separator = new View(this);
+                separator.setBackgroundColor(Color.BLACK);
+                GridLayout.LayoutParams sepParams = new GridLayout.LayoutParams();
+                sepParams.width = GridLayout.LayoutParams.MATCH_PARENT;
+                sepParams.height = 2    ; // 2 pixels high separator
+                sepParams.columnSpec = GridLayout.spec(0, 4); // Span across all 4 columns
+                sepParams.setMargins(0, 18, 0, 8); // Add top and bottom margin to separate the lines from the text
+                gridLayout.addView(separator, sepParams);
+            }
+        }
+
+
+
+}
 
     private void togglePopupMenu(View view, boolean isArrowUp, ImageView arrowIcon, int menuRes) {
         if (!isArrowUp) {
@@ -89,4 +144,7 @@ public class EventDetailActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
